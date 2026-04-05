5f508d3ac4bc3651db347392a552a5700b5e4e41
Luiz <lscfogaca@gmail.com>
2026-04-05 10:59:45 -0300

    mudança na lógica das funções utilizando calculo duplo, agora não usam mais for 1000 vezes

--- TabelaHash.cpp
+++ TabelaHash.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 #include <ctime>
 #include "tabelaHash.h"
+using namespace std;
 
 TabelaHash::TabelaHash(){
-    for(int i= 0; i < 1000; i++){
+    for(int i= 0; i < 10000; i++){
         tabelaHash[i].set_id(-1);
     }
 }
@@ -14,13 +15,12 @@ int TabelaHash::get_contCol(){
 
 void TabelaHash::insert(Sensor sensor){//insert na tabela utilizando o calculo de sondagem dupla
     int i = 0;
-    while(i < 1000){
-        int M = 1000;
+    while(i<10000){
+        int M = 10000;
         int h1 = sensor.get_id() % M;
         int h2 = 1+(sensor.get_id() % (M -1));
         int pos = (h1 + i*h2) % M;
-
-        if(tabelaHash[pos].get_id() == -1){
+        if(tabelaHash[pos].get_id() == -1 || tabelaHash[pos].get_id() == -11){
             tabelaHash[pos] = sensor;
             return;
         }else{
@@ -28,76 +28,99 @@ void TabelaHash::insert(Sensor sensor){//insert na tabela utilizando o calculo d
             contCol++;
         }
     }
+    cout<<"Tabela cheia"<<endl<<endl;
 }
 
 bool TabelaHash::existeId(int id){
-    for(int i = 0; i < 1000; i++){
-        if(tabelaHash[i].get_id() == id){
+    int i = 0;
+    while(i < 10000){
+        int M = 10000;
+        int h1 = id % M;
+        int h2 = 1+(id % (M -1));
+        int pos = (h1 + i*h2) % M;
+
+        if(tabelaHash[pos].get_id() == id){
             return true;
-        }
+        }else if(tabelaHash[pos].get_id() == -1)
+            return false;
+        else
+            i++;
     }
     return false;
 }
 
 void TabelaHash::search(int id){
-    for(int i = 0; i<1000; i++){
-        int M = 1000;
+    int i = 0;
+    while(i< 10000){
+        int M = 10000;
         int h1 = id % M;
         int h2 = 1+ (id % (M -1));
         int pos = (h1 + i*h2) % M;
 
         if(tabelaHash[pos].get_id() == id){
             tabelaHash[pos].exibir();
-            cout<<"pos:"<< pos;
+            cout<<"pos:"<< pos<<endl<<endl;
             return;
-        }
+        }else if(tabelaHash[pos].get_id() == -11)
+            i++;
+        else if(tabelaHash[pos].get_id() == -1){
+            cout<<"Não há nenhum sensor com o id: "<<id<<endl<<endl;
+            return;
+        }else 
+            i++;
     }
     cout<<"Não há nenhum sensor com o id: "<<id;
 }
 
 void TabelaHash::deletar(int id){
-    for(int i = 0; i<1000; i++){
-        int M = 1000;
+    int i =0;
+    while(i< 10000){
+        int M = 10000;
         int h1 = id % M;
         int h2 = 1+ (id % (M -1));
         int pos = (h1 + i*h2) % M;
 
         if(tabelaHash[pos].get_id() == id){
-            tabelaHash[pos].set_id(-1);
-            cout<<"sensor deletado com sucesso"<<endl;
+            tabelaHash[pos].set_id(-11);
+            cout<<"sensor deletado com sucesso"<<endl<<endl;
             return;
-        }
+        }else if(tabelaHash[pos].get_id() == -11)
+            i++;
+        else if(tabelaHash[pos].get_id() == -1){
+            cout<<"Não há nenhum sensor com esse id"<<endl<<endl;
+            return;
+        }else
+            i++;
     }
-    cout<<"Não há nenhum sensor com esse id"<<endl;
+    cout<<"Não há nenhum sensor com esse id"<<endl<<endl;
 }
 
-void TabelaHash::atualizarInfo(int id){
-    for(int i = 0; i<1000; i++){
-        int M = 1000;
+void TabelaHash::atualizarInfo(int id, float leitura){
+    int i = 0;
+    while(i<10000){
+        int M = 10000;
         int h1 = id % M;
         int h2 = 1+ (id % (M -1));
         int pos = (h1 + i*h2) % M;
 
         if(tabelaHash[pos].get_id() == id){
-            time_t now = time(nullptr);
-            tm *ltm = localtime(&now);
-
-            int hours = ltm->tm_hour;
-            int minutes = ltm->tm_min;
-
-            float decimalTime = hours + (static_cast<float>(minutes) / 60.0f);
-
-            tabelaHash[pos].set_ultimaLeitura(decimalTime);
-            cout<<"sensor atualizado com última leitura com sucesso"<<endl;
+            tabelaHash[pos].set_ultimaLeitura(leitura);
+            cout<<"sensor atualizado com sucesso"<<endl<<endl;
             return;
-        }
+        }else if(tabelaHash[pos].get_id() == -11)
+            i++;
+        else if(tabelaHash[pos].get_id() == -1){
+            cout<<"Não há nenhum sensor com esse id para atualizar"<<endl<<endl;
+            return;
+        }else
+            i++;
     }
-    cout<<"Não há nenhum sensor com esse id para atualizar"<<endl;
+    cout<<"Não há nenhum sensor com esse id para atualizar"<<endl<<endl;
 }
 
 void TabelaHash::exibirTabelaHash(){
-    for(int i = 0; i< 1000; i++){
-        if(tabelaHash[i].get_id() != -1){
+    for(int i = 0; i< 10000; i++){
+        if(tabelaHash[i].get_id() != -1 && tabelaHash[i].get_id() != -11){
             cout <<"Sensor["<<i<<"]: "<< " id: "<<tabelaHash[i].get_id() <<" tipo: " << tabelaHash[i].get_tipo()<<
             " localização: "<<tabelaHash[i].get_localizacao()<< " última leitura: " << tabelaHash[i].get_ultimaLeitura()<< endl;
         }
--- tabelaHash.h
+++ tabelaHash.h
@@ -6,13 +6,13 @@ using namespace std;
 class TabelaHash{ //Estático com 1000 pags - fechado utiliza cálculo duplo
     private:
         int contCol = 0;
-        Sensor tabelaHash[1000];
+        Sensor tabelaHash[10000];
     public:
         TabelaHash();
         void insert(Sensor sensor);
         void search(int id);
         void deletar(int id);
-        void atualizarInfo(int id);
+        void atualizarInfo(int id, float leitura);
         void exibirTabelaHash();
         int get_contCol();
         bool existeId(int id);
